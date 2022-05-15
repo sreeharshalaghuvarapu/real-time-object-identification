7f64e37f147189509bad3c366a312a5420469a13
sreeharshalaghuvarapu <72685506+sreeharshalaghuvarapu@users.noreply.github.com>
2022-05-15 18:44:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video ="";
-
+object = [];
+status ="";
 function preload(){
     video = createVideo('video.mp4');
     video.hide();
@@ -8,10 +9,35 @@ function preload(){
 function setup() {
     canvas = createCanvas(480,380);
     canvas.center();
-}
+} 
 
 function draw() {
     image(video, 0, 0, 480, 380);
+    if(status != "")
+    {
+        objectDetector.detect(video,gotResult);
+        for (i = 0; i < object.length; i++) {
+            document.getElementById("status").innerHTML = "status : objects detected";
+            document.getElementById("number_of_ objects").innerHTML = "number of objects dected are :"+ object.length;
+
+            fill("#FF0000")
+            percent = floor(objects[i].confidence * 100)
+            text (objects[i].label + " "  + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill()
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height,)
+        }
+
+    }
+        function gotResult(error, results) {
+            if (error) {
+                console.log(error);
+            }
+                console.log(results);
+                objects = results;
+        }
+   
+    
 }
 
 function start()
